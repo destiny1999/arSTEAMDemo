2b557ed573c0b03d7381b71a0b7132bc67f06b2a
destiny1999 <asd94073@gmail.com>
2021-10-05 18:42:29 +0800

    beforeChangeColoring

---
+++ Assets/RecognizeTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RecognizeTest : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    // Start is called before the first frame update
+    [SerializeField] SampleSpeechToText sample;
+    [SerializeField] InputField message = null;
+    HashSet<string> targetName = new HashSet<string>();
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        sample.StartRecording();
+
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        //sample.StartRecording();
+        //message.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
